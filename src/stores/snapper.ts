5e85abed09d32b2f0d943ac7fb6bea0109fdba8e
Bo Vandersteene <1673799+bovandersteene@users.noreply.github.com>
2024-10-08 15:46:12 +0200

    fix: filter (#2)
    * fix: filter
    
    * fix: update store

--- src/stores/snapper.ts
+++ src/stores/snapper.ts
@@ -1,6 +1,3 @@
-import { type Line } from "@ghentcdh/vue-component-annotated-text";
-import { tokenize } from "./../text_utilities";
-
 export interface Snapper {
   fixOffset(newStart: number, newEnd: number): { start: number; end: number };
 }
