21f3191db5e60594ba714430a78e600169fb1038
Bo Vandersteene <bo.vandersteene@firepeak.tech>
2024-10-22 09:28:40 +0200

    chore: update annotation component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false,
   theme: {
     extend: {
       fontSize: {
-        base: '0.875rem', // 14px
-      }
-    }  },
+        base: "0.875rem", // 14px
+      },
+    },
+  },
+  daisyui: {
+    themes: ["emerald"],
+  },
   plugins: [
-    require("@tailwindcss/typography"),  
-    require('daisyui'),
-  ]
-}
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    require("@tailwindcss/typography"),
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    require("daisyui"),
+  ],
+};
