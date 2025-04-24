3974c3963b6749792b5a3ec0bf345f4fdb3db54d
Bo Vandersteene <bo@firepeak.tech>
2025-04-24 18:31:14 +0200

    feat: show metadata on double click

---
+++ src/components/AnnotationMetadata.vue
@@ -0,0 +1,18 @@
+<template>
+  <p class="py-2">
+    <strong class="underline">LineLinguisticCharacteristic:</strong>
+    {{ annotation.metadata?.text }}
+  </p>
+  <strong class="underline">Metadata</strong>
+  <ul class="text-sm text-content">
+    <li v-for="property of annotation.properties" :key="property.id_name">
+      <strong> {{ property.label }}:</strong> {{ property.name }}
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import type { RuleAnnotation } from "@/types/Annotation";
+
+defineProps<{ annotation: RuleAnnotation }>();
+</script>
---
+++ src/components/selected-annotation.vue
@@ -0,0 +1,20 @@
+<template>
+  <AnnotatedText
+    :annotations="[annotation]"
+    :lines="getAnnotatedLines(textLines, annotation.start, annotation.end).lines"
+    :allow-edit="false"
+  />
+  <AnnotationMetadata :annotation="annotation" />
+</template>
+
+<script setup lang="ts">
+import { AnnotatedText, type Line } from "@ghentcdh/vue-component-annotated-text";
+import AnnotationMetadata from "./AnnotationMetadata.vue";
+import { getAnnotatedLines } from "../utils/annotation_utils";
+import type { RuleAnnotation } from "@/types/Annotation";
+
+defineProps<{
+  textLines: Line[];
+  annotation: RuleAnnotation;
+}>();
+</script>
