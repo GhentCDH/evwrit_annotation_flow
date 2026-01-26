04d60c2d754804f66129e723215bfb40382504a8
Bo Vandersteene <bo@firepeak.tech>
2026-01-26 20:15:43 +0100

    feat: add keycloak

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <main>
-    <RouterView />
-  </main>
+  <div class="flex flex-col h-screen">
+    <AppHeader />
+    <main class="flex-1 overflow-auto">
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import { usePingStore } from "./stores/ping.state";
+import AppHeader from "./components/AppHeader.vue";
 
 // Don't remove a small hack to stay alive with the app
 usePingStore();
