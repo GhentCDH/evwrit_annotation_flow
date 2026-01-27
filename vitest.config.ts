e41b77918dab8eb8d469813e54e4ecc7efec4376
Bo Vandersteene <bo@firepeak.tech>
2026-01-27 09:18:48 +0100

    fix: build

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,16 @@
-import { mergeConfig, defineConfig, configDefaults } from "vitest/config";
+import { defineConfig, configDefaults } from "vitest/config";
 import { fileURLToPath } from "node:url";
-import viteConfig from "./vite.config";
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: "jsdom",
-      globals: true,
-      exclude: [...configDefaults.exclude, "e2e/**"],
-      root: fileURLToPath(new URL("./", import.meta.url)),
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@/types": fileURLToPath(new URL("./src/types", import.meta.url)),
     },
-  }),
-);
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    exclude: [...configDefaults.exclude, "e2e/**"],
+    root: fileURLToPath(new URL("./", import.meta.url)),
+  },
+});
